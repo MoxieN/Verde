f5597292e39b5527ca0eae25cba022014e265b25
Kiirx <43090601+Kiirx@users.noreply.github.com>
2022-05-12 15:57:12 +0200

    Base of Verde (READ DESC)
    It is not a really functionnal base but its to start creating other functions

---
+++ Verde/Manager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using Cosmos.System.Network.IPv4;
+using Cosmos.System.Network.IPv4.TCP;
+
+namespace VerdeKernel.Libraries.Verde // Change this to where you store this file
+{
+    public class Manager
+    {
+        public string Install(string address, string port, string message, string chs) 
+        {
+            /* This features a TCP connection
+             network initialization is needed*/
+
+            // Parse arguments
+            Address add = Address.Parse(address);
+            int destPort = Int32.Parse(port);
+
+            // Base local port = 4242
+            Console.WriteLine("Connection to destination host");
+            using var xClient = new TcpClient(destPort); // Ports should be corresponding
+            xClient.Connect(add, destPort);
+
+            // Send data
+            Console.WriteLine("Sending request...");
+            xClient.Send(Encoding.ASCII.GetBytes(message));
+
+            // Receive data
+            var endpoint = new EndPoint(Address.Zero, 0);
+            Console.WriteLine("EndPoint set");
+            
+            var data = xClient.Receive(ref endpoint);  //set endpoint to remote machine IP:port
+            Console.WriteLine("Received!");
+
+            return Encoding.UTF8.GetString(data);
+        }
+    }
+}
